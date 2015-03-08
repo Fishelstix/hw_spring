73ce3935d3fb7050bbed2ec12732d28a0ad5f4d7
Max Fishelson <maxwellkf@aol.com>
2015-03-08 18:19:13 -0400

    HW5

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -1,27 +1,36 @@
-public class Selection(){
-    private int[] list;
-    public Selection(int list){
-        this.list=list;
-    }
-    public int[] pivot(int SI, int EI){
+public class Selection{
+    public int select(int[] list, int index_desired, int low, int high){
         int[] D = new int[list.length];
-        for(int i=0; i<SI; i++){
+        for(int i=0; i<low; i++){
             D[i]=list[i];
         }
-        for(int i=EI; i<list.length; i++){
+        for(int i=high+1; i<list.length; i++){
             D[i]=list[i];
         }
-        int p = list[SI];
-        for(int i=SI+1; i<EI; i++){
-            if(list[i]<p){
-                D[SI]=list[i];
-                SI++;
+        int pivot = list[low];
+        int nlow = low;
+        int nhigh = high;
+        for(int i=low+1; i<=high; i++){
+            if(list[i]<pivot){
+                D[nlow]=list[i];
+                nlow++;
             }else{
-                D[EI-1]=list[i];
-                EI--;
+                D[nhigh]=list[i];
+                nhigh--;
             }
         }
-        D[SI]=p;
-        return D;
+        D[nlow]=pivot;
+        if(nlow==index_desired){
+            return pivot;
+        }else if(nlow<index_desired){
+            return select(D, index_desired, nlow+1, high);
+        }else{
+            return select(D, index_desired, low, nlow-1);
+        }
+    }
+    public static void main(String[] args){
+        int[] l = {20, 13, 16, 14, 18, 11, 19, 15, 12, 17};
+        Selection s = new Selection();
+        System.out.println(s.select(l, 5, 0, l.length-1));
     }
 }

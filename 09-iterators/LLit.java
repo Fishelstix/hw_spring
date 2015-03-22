7b81d9b46fe9b411e018ca6f7b93e5045607f7df
Max Fishelson <maxwellkf@aol.com>
2015-03-22 15:03:47 -0400

    HW9

--- 09-iterators/LLit.java
+++ 09-iterators/LLit.java
@@ -3,22 +3,25 @@ import java.util.*;
 
 public class LLit<E> implements Iterator<E>{
     private Node<E> t, tSeed;
-    public LLit(Node<E> n, Node<E> nSeed){
+    public LLit(Node<E> n){
 	t=n;
-	tSeed=nSeed;
+	tSeed=n;
     }
-    
+
     public boolean hasNext(){
 	return t!=null;
     }
-    
+
     public E next(){
 	E retval = t.getData();
 	t=t.getNext();
+	if(!(tSeed==t || tSeed.getNext() == t)){
+	    tSeed=tSeed.getNext();
+	}
 	return retval;
     }
-    
+
     public void remove(){
-	// to be completed for homework
+	tSeed.setNext(t);
     }
 }
